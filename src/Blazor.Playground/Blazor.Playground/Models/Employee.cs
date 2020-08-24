9cdbc9fdb979d88f63bd07e875c3bd0477624138
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2020-08-24 10:24:14 -0700

    Add random sample code

---
+++ src/Blazor.Playground/Blazor.Playground/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.Playground.Models
+{
+    public class Employee
+    {
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long.")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime HireDate { get; set; }
+        public bool IsTerminated { get; set; }
+    }
+}
